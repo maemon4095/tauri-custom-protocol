578d7b1a6b864ced60fbf8ebb9e71f2fc6dda9f1
maemon4095 <maemondecocted@gmail.com>
2023-10-28 12:52:57 +0900

    blocking

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,15 @@ fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
         .register_uri_scheme_protocol("mybinary", |app, req| {
-            let now = chrono::Local::now();
-            let now = format!("Request received at: {}", now.to_rfc3339());
+            tauri::async_runtime::block_on(async {
+                tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+                let now = chrono::Local::now();
+                let now = format!("Request received at: {}", now.to_rfc3339());
 
-            tauri::http::ResponseBuilder::new()
-                .header("Access-Control-Allow-Origin", "*")
-                .body(now.as_bytes().to_vec())
+                tauri::http::ResponseBuilder::new()
+                    .header("Access-Control-Allow-Origin", "*")
+                    .body(now.as_bytes().to_vec())
+            })
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
